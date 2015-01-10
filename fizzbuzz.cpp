7c32634df4cf7199c9951b04ad883897e21747f1
Timothy Boss <bosst@wit.edu>
2015-01-09 21:53:51 -0500

    Git practice

---
+++ fizzbuzz.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int i = 0;
+	for (i = 1; i < 100; i++)
+	{
+		if (((i % 3) == 0) && ((i % 5) == 0))
+		{
+			cout << "FizzBuzz" << endl;
+		}
+		else if ((i % 5) == 0)
+		{
+			cout << "Buzz" << endl;
+		}
+		else if ((i % 3) == 0)
+		{
+			cout << "Fizz" << endl;
+		}
+		else
+		{
+			cout << i << endl;;
+		}
+	}
+	system("pause");
+	return 0;
+}
